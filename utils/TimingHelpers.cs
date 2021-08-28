6802401da1eed9be10b84c40eec13bd2cad520f3
nikolaus07 <claus@thevessen.ch>
2021-08-28 18:43:02 +0200

    add puppetersharp

---
+++ utils/TimingHelpers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace specf1.utils
+{
+    public class TimingHelpers
+    {
+        public static TimeSpan vergangeneZeit(DateTime startZeit)
+        {
+            return TimeSpan.FromSeconds((DateTime.Now - startZeit).TotalSeconds);
+        }
+
+       
+    }
+}

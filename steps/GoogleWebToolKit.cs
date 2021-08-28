37a7cdef7c77b53269fd3671e0d65718fc07d925
nikolaus07 <claus@thevessen.ch>
2021-08-28 10:34:35 +0200

    update

---
+++ steps/GoogleWebToolKit.cs
@@ -0,0 +1,19 @@
+﻿using TechTalk.SpecFlow;
+using specf1.TaschenRechner;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace specf1.steps {
+    [Binding]
+
+    public class GoogleWebToolKit    
+    {
+
+
+     
+
+        [Then(@"Ergebnis der Subtraktion sollte '(.*)'")]
+        public void ThenErgebnisDerSubtraktionSollte(int p0) {
+            Assert.AreEqual(p0, Rechner.Ergebnis);
+        }
+    }
+}
